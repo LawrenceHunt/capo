48d6ee7f3da11233b88d71059af90c9efa24bc65
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-11-19 19:22:05 +0000

    using re-base instead of firebase - syncState rocks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
-import fire from './fire';
+// import fire from './fire';
+import {base} from './base'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { messages: [] }; // <- set up react state
   }
-  componentWillMount(){
-    /* Create reference to messages in Firebase Database */
-    let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
-    messagesRef.on('child_added', snapshot => {
-      /* Update React state when message is added at Firebase Database */
-      let message = { text: snapshot.val(), id: snapshot.key };
-      this.setState({ messages: [message].concat(this.state.messages) });
+  // componentWillMount(){
+  //   /* Create reference to messages in Firebase Database */
+  //   let messagesRef = db.ref('messages').orderByKey().limitToLast(100);
+  //   messagesRef.on('child_added', snapshot => {
+  //     /* Update React state when message is added at Firebase Database */
+  //     let message = { text: snapshot.val(), id: snapshot.key };
+  //     this.setState({ messages: [message].concat(this.state.messages) });
+  //   })
+  // }
+
+  componentWillMount() {
+    this.ref = base.syncState('messages', {
+      context: this,
+      state: 'messages',
+      asArray: true
     })
   }
-  addMessage(e){
-    e.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the message to Firebase */
-    fire.database().ref('messages').push( this.inputEl.value );
-    this.inputEl.value = ''; // <- clear the input
+
+  addMessage(e) {
+    e.preventDefault()
+    const messages = [...this.state.messages]
+    const timestamp = Date.now()
+    messages.push({
+      text: this.inputEl.value,
+      id: timestamp
+    })
+    this.setState({messages})
+    this.inputEl.value = ''
   }
+
+  // addMessage(e){
+  //   e.preventDefault(); // <- prevent form submit from reloading the page
+  //   /* Send the message to Firebase */
+  //   db.ref('messages').push( this.inputEl.value );
+  //   this.inputEl.value = ''; // <- clear the input
+  // }
   render() {
     return (
       <form onSubmit={this.addMessage.bind(this)}>
