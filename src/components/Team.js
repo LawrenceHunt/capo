1310f2a152f009c2f534ffd8e730da51dde08f40
cloudheadboatman <adam.boatman@gmail.com>
2017-12-03 23:11:34 +0000

    Changes to Team Page

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,37 +1,70 @@
 import React from 'react';
 
 class Team extends React.Component {
-  render() {
+
+  generateCards () {
     return (
-      <div className="team">
-        <div className="squad-list">
-          <div className="player">Player One</div>
-          <div className="player">Player Two</div>
-          <div className="player">Player Three</div>
-          <div className="player">Player Four</div>
-          <div className="player">Player Five</div>
-          <div className="player">Player Six</div>
-          <div className="player">Player Seven</div>
-          <div className="player">Player Eight</div>
-          <div className="player">Player Nine</div>
-          <div className="player">Player Ten</div>
-          <div className="player">Player Eleven</div>
-          <div className="player">Player Twelve</div>
-          <div className="player">Player Thirteen</div>
-          <div className="player">Player Fourteen</div>
-          <div className="player">Player Fifteen</div>
+      <div className="team-grid">
+        <div className="player-card">
+          <div className="content">
+            <div className="front">
+              Lozza
+              <div><img className="avatar" src="http://gallery.rxmuscle.com/newgallery/DSC_5240_SXGJOXLGXL.jpg" /></div>
+            </div>
+            <div className="back">
+              Stats
+            </div>
+          </div>
         </div>
-        <div className="bio">
-          <header className="player-name">Lawrence Hunt</header>
-          <div className="player-picture"><span>Image</span></div>
-          <ul className="player-stats">
-            <li>Games Played: TAKE IN GAME DATA</li>
-            <li>Goals Scored: TAKE IN GAME DATA</li>
-            <li>Assists Made: TAKE IN GAME DATA</li>
-            <li>Man of Match Awards: TAKE IN GAME DATA</li>
-          </ul>
+        <div className="player-card">
+          <div className="content">
+            <div className="front">
+              AdBalls
+            </div>
+            <div className="back">
+              Stats
+            </div>
+          </div>
+        </div>
+        <div className="player-card">
+          <div className="content">
+            <div className="front">
+              Jezza
+            </div>
+            <div className="back">
+              Stats
+            </div>
+          </div>
+        </div>
+        <div className="player-card">
+          <div className="content">
+            <div className="front">
+              Greek
+            </div>
+            <div className="back">
+              Stats
+            </div>
+          </div>
+        </div>
+        <div className="player-card">
+          <div className="content">
+            <div className="front">
+              PooFace
+            </div>
+            <div className="back">
+              Stats
+            </div>
+          </div>
         </div>
       </div>
+    )
+  }
+
+  render() {
+    return (
+        <div className="team">
+          {this.generateCards()}
+        </div>
     )
   }
 }
