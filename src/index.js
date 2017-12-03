761b33a590a1cfaf6e3c9213598a7f8f7cb2ca4c
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-12-03 11:46:27 +0000

    react router dom added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory'
+// css
 import './index.css';
+// components
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Team from './components/Team'
+// import NotFound from './components/NotFound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory()
+
+const routes = () => (
+  <Router history={history}>
+    <App />
+  </Router>
+)
+
+// <Miss component={NotFound}></Miss>
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
