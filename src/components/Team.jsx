a48c5a41cf1a730f5be5f86e83015a01eacebb70
cloudheadboatman <adam.boatman@gmail.com>
2017-12-04 08:46:00 +0000

    Changes to player cards - you're looking good Loz ;)

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome'
 
 class Team extends React.Component {
 
@@ -9,10 +10,17 @@ class Team extends React.Component {
           <div className="content">
             <div className="front">
               Lozza
-              <div><img className="avatar" src="http://gallery.rxmuscle.com/newgallery/DSC_5240_SXGJOXLGXL.jpg" /></div>
+              <div><img alt="Lozza" className="avatar" src="http://gallery.rxmuscle.com/newgallery/DSC_5240_SXGJOXLGXL.jpg" /></div>
             </div>
             <div className="back">
               Stats
+              <ul className="stats">
+                <li>Games Played: 10</li>
+                <li>PoM Awards: 3</li>
+                <li>Goals Scored: 55</li>
+                <li>Assists Made: 20</li>
+              </ul>
+              <FontAwesome className="mvp" name="star" />
             </div>
           </div>
         </div>
